cb30dfbe024085f87d437f350a18ebeef7183f26
InLyeol <azrop2@gmail.com>
2020-10-05 08:48:59 +0900

    2020-10-05 home commit

--- app/src/main/java/kr/core/bowwow/fragments/Chatting.java
+++ app/src/main/java/kr/core/bowwow/fragments/Chatting.java
@@ -3,6 +3,7 @@ package kr.core.bowwow.fragments;
 import android.app.Activity;
 import android.content.Context;
 import android.content.Intent;
+import android.graphics.Rect;
 import android.media.AudioManager;
 import android.media.MediaPlayer;
 import android.media.audiofx.Visualizer;
@@ -12,10 +13,12 @@ import android.os.Bundle;
 import android.os.Handler;
 import android.os.Looper;
 import android.util.Log;
+import android.util.TypedValue;
 import android.view.LayoutInflater;
 import android.view.MotionEvent;
 import android.view.View;
 import android.view.ViewGroup;
+import android.view.ViewTreeObserver;
 import android.view.inputmethod.InputMethodManager;
 import android.widget.Toast;
 
@@ -163,14 +166,47 @@ public class Chatting extends Fragment implements View.OnClickListener {
 
 
 
-
         binding.areaListeningAll.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View v) {
 
             }
         });
+
+        setKeyboardVisibilityListener();
+
         return binding.getRoot();
+    }
+
+    private void setKeyboardVisibilityListener() {
+        final View parentView = ((ViewGroup) act.findViewById(android.R.id.content)).getChildAt(0);
+        parentView.getViewTreeObserver().addOnGlobalLayoutListener(new ViewTreeObserver.OnGlobalLayoutListener() {
+
+            private boolean alreadyOpen;
+            private final int defaultKeyboardHeightDP = 100;
+            private final int EstimatedKeyboardDP = defaultKeyboardHeightDP + (Build.VERSION.SDK_INT >= Build.VERSION_CODES.LOLLIPOP ? 48 : 0);
+            private final Rect rect = new Rect();
+
+            @Override
+            public void onGlobalLayout() {
+                int estimatedKeyboardHeight = (int) TypedValue.applyDimension(TypedValue.COMPLEX_UNIT_DIP, EstimatedKeyboardDP, parentView.getResources().getDisplayMetrics());
+                parentView.getWindowVisibleDisplayFrame(rect);
+                int heightDiff = parentView.getRootView().getHeight() - (rect.bottom - rect.top);
+                boolean isShown = heightDiff >= estimatedKeyboardHeight;
+
+                if (isShown == alreadyOpen) {
+                    Log.i("Keyboard state", "Ignoring global layout change...");
+                    return;
+                }
+                alreadyOpen = isShown;
+
+                if (isShown) {
+                    binding.areaListening.setVisibility(View.GONE);
+                } else {
+                    binding.areaListening.setVisibility(View.VISIBLE);
+                }
+            }
+        });
     }
 
     @Override
@@ -243,6 +279,8 @@ public class Chatting extends Fragment implements View.OnClickListener {
             dogTrans.putExtra("td_run_time", String.valueOf(mp.getDuration()));
             startActivity(dogTrans);
         }
+
+        app.isTrans = false;
 
 //        mp.setOnCompletionListener(new MediaPlayer.OnCompletionListener() {
 //            @Override
